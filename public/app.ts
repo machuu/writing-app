eb3721bafc83f46430bcc6c9e89bd65a9954ee0f
Matt Hoover <matt@hoover.ml>
2019-09-23 06:15:05 -0400

    Add `loadButton`
    refs #9

--- public/app.ts
+++ public/app.ts
@@ -1,4 +1,6 @@
 const saveButton = document.getElementById('saveButton');
+const loadButton = document.getElementById('loadButton');
+
 const EditorJS = require('@editorjs/editorjs');
 var editor = new EditorJS({
   data: {
@@ -17,7 +19,16 @@ saveButton.addEventListener('click', function () {
   editor.save().then((savedData: any) => {
     console.log("Saving EditorJS");
     console.log(savedData);
+    localStorage.setItem('editorjs',JSON.stringify(savedData));
   });
+});
+
+loadButton.addEventListener('click', function () {
+  console.log('Retreiving EditorJS');
+  var retreived_editorjs = JSON.parse( localStorage.getItem('editorjs') );
+  console.log('retreived_editorjs: ', retreived_editorjs );
+  // Assign retreived data to editor object
+  editor.render(retreived_editorjs);
 });
 
 /**
