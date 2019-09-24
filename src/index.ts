0d2465db764d7b8489da601e477c5ac1af0864fd
Matt Hoover <matt@hoover.ml>
2019-09-24 06:06:10 -0400

    Re-combine `src/index.ts`
    importing separate files requires declaring them as modules:
    
    1. That's a big learning curve
    2. The service worker registration should be as close to the top as
    possible.
    
    For now, we can just keep it as one TS file, and break that out into a
    module when it becomes necessary
    
    refs #7

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,60 @@
-import registerServiceWorker = require(registerServiceWorker);
-import mainEditor = require(mainEditor);
+/**
+ * Service worker registration
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+
+    /**
+     * You can call register() every time a page loads without concern;
+     * the browser will figure out if the service worker is already registered or not and handle it accordingly.
+     */
+    navigator.serviceWorker.register('./serviceWorker.js', {scope : "./"} ).then(function(registration) {
+
+      // everything is ok
+      console.log('ServiceWorker registration success, scope: ', registration.scope);
+
+    }, function(err) {
+      // an error occurred
+      console.log('ServiceWorker registration failed: ', err);
+    });
+
+
+  });
+}
+
+/**
+ * Text Editor
+ */
+const saveButton = document.getElementById('saveButton');
+const loadButton = document.getElementById('loadButton');
+
+const EditorJS = require('@editorjs/editorjs');
+var editor = new EditorJS({
+  data: {
+    blocks: [
+      {
+      type: "paragraph",
+        data: {
+          text: "Sample Text. You can delete and overwrite this"
+        }
+      }
+    ]
+  }
+});
+
+saveButton.addEventListener('click', function () {
+  editor.save().then((savedData: any) => {
+    console.log("Saving EditorJS");
+    console.log(savedData);
+    localStorage.setItem('editorjs',JSON.stringify(savedData));
+  });
+});
+
+loadButton.addEventListener('click', function () {
+  console.log('Retreiving EditorJS');
+  var retreived_editorjs = JSON.parse( localStorage.getItem('editorjs') );
+  console.log('retreived_editorjs: ', retreived_editorjs );
+  // Assign retreived data to editor object
+  editor.render(retreived_editorjs);
+});
+
