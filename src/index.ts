2fb2d206907f51755a7b6a95ccb73fda7d5be92d
Matt Hoover <matt@hoover.ml>
2019-09-25 06:13:07 -0400

    Automatically load saved data from localStorage, if it exists
    refs #9

--- src/index.ts
+++ src/index.ts
@@ -29,30 +29,44 @@ const saveButton = document.getElementById('saveButton');
 const loadButton = document.getElementById('loadButton');
 
 const EditorJS = require('@editorjs/editorjs');
-var editor = new EditorJS({
-  data: {
-    blocks: [
-      {
-      type: "paragraph",
-        data: {
-          text: "Sample Text. You can delete and overwrite this"
-        }
+
+const editorjs_storage_item = "editorjs_data";
+let editorjs_data = {};
+
+let default_editorjs_data={
+  blocks: [
+    {
+    type: "paragraph",
+      data: {
+        text: "Sample Text. You can delete and overwrite this"
       }
-    ]
-  }
+    }
+  ]
+};
+
+if ( localStorage.getItem(editorjs_storage_item) === null ) {
+  console.log(`item '${editorjs_storage_item}' is not in localStorage`)
+  editorjs_data = default_editorjs_data;
+} else {
+  console.log(`item '${editorjs_storage_item}' is in localStorage`)
+  editorjs_data = JSON.parse( localStorage.getItem(editorjs_storage_item) );
+}
+
+let editor = new EditorJS({
+  data: editorjs_data
 });
 
 saveButton.addEventListener('click', function () {
   editor.save().then((savedData: any) => {
-    console.log("Saving EditorJS");
+    console.log(`Saving EditorJS: ${editorjs_storage_item}`);
     console.log(savedData);
-    localStorage.setItem('editorjs',JSON.stringify(savedData));
+    localStorage.setItem(editorjs_storage_item,JSON.stringify(savedData));
   });
 });
 
 loadButton.addEventListener('click', function () {
-  console.log('Retreiving EditorJS');
-  var retreived_editorjs = JSON.parse( localStorage.getItem('editorjs') );
+  console.log(`Retreiving EditorJS from localStorage: ${editorjs_storage_item}`);
+  var retreived_editorjs = JSON.parse( localStorage.getItem(editorjs_storage_item) );
   console.log('retreived_editorjs: ', retreived_editorjs );
   // Assign retreived data to editor object
   editor.render(retreived_editorjs);
