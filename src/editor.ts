b1ea3b82e18084a9d6b417b611151dc1ba60a149
Matt Hoover <matt@hoover.ml>
2019-10-10 07:54:35 -0400

    Refactor Main Editor into TextEditor class in `src/editor.ts`

---
+++ src/editor.ts
@@ -0,0 +1,147 @@
+/**
+ * Text Editor
+ */
+const EditorJS = require('@editorjs/editorjs');
+
+var defaultTextEditorData: any = {
+  blocks: [
+    {
+      type: "paragraph",
+      data: {
+        text: "Sample Text. You can delete and overwrite this"
+      }
+    }
+  ]
+};
+
+class TextEditor {
+
+  private editor: any;
+  private defaultElementId: string = "editorjs";
+  private elementId: string;
+  private _element: HTMLElement;
+  private defaultDataId: string = "textEditorData";
+  private defaultData: any;
+  private dataId: string;
+  private _data: any = {};
+
+  constructor(elementId: string = null, initData: any = defaultTextEditorData ) {
+    if ( elementId == null ) {
+      this.elementId = this.defaultElementId;
+    } else {
+      this.elementId = elementId;
+    }
+
+    if ( document.getElementById(this.elementId) ) {
+      this.element = document.getElementById(this.elementId);
+    } else {
+      console.log(`Unable to Initialize Text Editor in Element with id '${this.elementId}'`);
+      return;
+    }
+
+    this.defaultData = defaultTextEditorData;
+    if ( initData == null ) {
+      initData = this.defaultData;
+    }
+
+    this.dataId = this.defaultDataId;
+
+    this.editor = new EditorJS({
+      holder: this.element.id,
+      data:     initData
+    });
+  }
+
+  public get element() {
+    return this._element;
+  }
+
+  public set element(givenElement: HTMLElement) {
+    if ( this._element == null ) {
+      this._element = givenElement;
+      console.log(`Setting Text Editor Element to element with id '${givenElement.id}'`);
+    } else {
+      console.log(`Can not change Text Editor element '${this.element.id}' -> '${givenElement.id}'`);
+    }
+  }
+
+  // Start new content
+  public reset(): void {
+    this.data = this.defaultData;
+    this.dataId = this.defaultDataId;
+  }
+
+  public addSaveButton(saveButtonId: string): void {
+    if( document.getElementById(saveButtonId) ) {
+      document.getElementById(saveButtonId).addEventListener('click', () => { this.save() });
+    }
+  }
+
+  public addLoadButton(loadButtonId: string): void {
+    if( document.getElementById(loadButtonId) ) {
+      document.getElementById(loadButtonId).addEventListener('click', () => { this.load() });
+    }
+  }
+
+  // retreive dataId from storage, then render in Text Editor
+  public load(dataId: string = "" ): void {
+    if ( dataId == "" ) {
+      dataId = this.promptForDataId();
+    }
+    //if ( this.dataIdExists(dataId) ) {
+      console.log(`Retreiving EditorJS from localStorage: ${dataId}`);
+      let retreivedData = JSON.parse( localStorage.getItem(dataId) );
+      console.log('retreived_data: ', retreivedData );
+      // Assign retreived data to editor object
+      this.editor.render(retreivedData);
+    //} else {
+      //console.log(`Unable to load '${dataId}' from storage`);
+    //}
+  }
+
+  // save data from Text Editor into dataId
+  public save(dataId: string = ""): void {
+    if ( dataId == "" ) {
+      dataId = this.promptForDataId();
+    }
+
+    //if ( !this.dataIdExists(dataId) || this.confirmOverwriteOld(dataId) ) {
+      this.editor.save().then((savedData: any) => {
+        console.log(`Saving Editor Data: ${dataId}`);
+        console.log(savedData);
+        localStorage.setItem(dataId,JSON.stringify(savedData));
+      });
+    //} else {
+      //alert("Cancelled saving current data into existing id: " + dataId );
+    //}
+  }
+
+  // return JSON object of Text Editor Data
+  public get data() {
+    return this._data;
+  }
+
+  // assign JSON object to Text Editor Data 
+  public set data(data: any) {
+    this._data = data;
+    this.renderData(this.data);
+  }
+
+  private confirmOverwriteOld(dataId: string): boolean {
+    return confirm(`Overwrite existing saved data with id '${dataId}'` );
+  }
+
+  private promptForDataId(): string {
+    return prompt("Enter ID for Editor Data:");
+  }
+
+  private dataIdExists(dataId: string) {
+    return ( localStorage.getItem(dataId) === null );
+  }
+
+  private renderData(data: any) {
+    this.editor.render(data);
+  }
+}
+
+export default TextEditor;
--- src/index.ts
+++ src/index.ts
@@ -35,53 +35,8 @@ let cardNavigator = new NavMenu('card-navigator');
 cardNavigator.closed_width = "20px";
 cardNavigator.opened_width = "250px";
 
-/**
- * Text Editor
- */
-const saveButton = document.getElementById('saveButton');
-const loadButton = document.getElementById('loadButton');
+import TextEditor from "./editor";
 
-const EditorJS = require('@editorjs/editorjs');
-
-const editorjs_storage_item = "editorjs_data";
-let editorjs_data = {};
-
-let default_editorjs_data={
-  blocks: [
-    {
-    type: "paragraph",
-      data: {
-        text: "Sample Text. You can delete and overwrite this"
-      }
-    }
-  ]
-};
-
-if ( localStorage.getItem(editorjs_storage_item) === null ) {
-  console.log(`item '${editorjs_storage_item}' is not in localStorage`)
-  editorjs_data = default_editorjs_data;
-} else {
-  console.log(`item '${editorjs_storage_item}' is in localStorage`)
-  editorjs_data = JSON.parse( localStorage.getItem(editorjs_storage_item) );
-}
-
-let editor = new EditorJS({
-  data: editorjs_data
-});
-
-saveButton.addEventListener('click', function () {
-  editor.save().then((savedData: any) => {
-    console.log(`Saving EditorJS: ${editorjs_storage_item}`);
-    console.log(savedData);
-    localStorage.setItem(editorjs_storage_item,JSON.stringify(savedData));
-  });
-});
-
-loadButton.addEventListener('click', function () {
-  console.log(`Retreiving EditorJS from localStorage: ${editorjs_storage_item}`);
-  var retreived_editorjs = JSON.parse( localStorage.getItem(editorjs_storage_item) );
-  console.log('retreived_editorjs: ', retreived_editorjs );
-  // Assign retreived data to editor object
-  editor.render(retreived_editorjs);
-});
-
+let mainEditor = new TextEditor('editorjs');
+mainEditor.addSaveButton('saveButton');
+mainEditor.addLoadButton('loadButton');
