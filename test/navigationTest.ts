20ca147dd3a4b941e2bf7272daa5e9ecfa537c2f
Matt Hoover <matt@hoover.ml>
2019-11-10 07:24:26 -0500

    test: Add failing test cases for Close/Open/Toggle Buttons
    refs #19
    refs

--- test/navigationTest.ts
+++ test/navigationTest.ts
@@ -34,6 +34,42 @@ describe("Creating a Navigator", () => {
 	  let navigator: NavMenu = new NavMenu(navigatorContainer.id);
     expect(navigator.element).to.equal(navigatorContainer);
   });
+
+  it("should auto assign an Close Button, if the right element id exists", () => {
+    let navigator: NavMenu = new NavMenu(navigatorContainer.id);
+    expect(navigator.close_button).to.equal(navigatorCloseButton);
+  });
+
+  it("should not auto assign an Close Button, if it the right element id doesn't exist", () => {
+		// Change id of button, so it doesn't match navigatorContainer.id
+		navigatorCloseButton.id = "foo";
+    let navigator: NavMenu = new NavMenu(navigatorContainer.id);
+    expect(navigator.close_button).to.equal(undefined);
+  });
+
+  it("should auto assign an Open Button, if the right element id exists", () => {
+    let navigator: NavMenu = new NavMenu(navigatorContainer.id);
+    expect(navigator.open_button).to.equal(navigatorOpenButton);
+  });
+
+  it("should not auto assign an Open Button, if it the right element id doesn't exist", () => {
+		// Change id of button, so it doesn't match navigatorContainer.id
+		navigatorOpenButton.id = "foo";
+    let navigator: NavMenu = new NavMenu(navigatorContainer.id);
+    expect(navigator.open_button).to.equal(undefined);
+  });
+
+  it("should auto assign an Toggle Button, if the right element id exists", () => {
+    let navigator: NavMenu = new NavMenu(navigatorContainer.id);
+    expect(navigator.toggle_button).to.equal(navigatorToggleButton);
+  });
+
+  it("should not auto assign an Toggle Button, if it the right element id doesn't exist", () => {
+		// Change id of button, so it doesn't match navigatorContainer.id
+		navigatorToggleButton.id = "foo";
+    let navigator: NavMenu = new NavMenu(navigatorContainer.id);
+    expect(navigator.toggle_button).to.equal(undefined);
+  });
 });
 
 /*
