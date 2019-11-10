cb4a8523d09bdb034dc5c3553865e7e2fb7001b5
Matt Hoover <matt@hoover.ml>
2019-11-10 11:02:46 -0500

    test: Add failing test for local object updating global `cards` object
    refs #21

--- test/decksTest.ts
+++ test/decksTest.ts
@@ -4,8 +4,38 @@ import "mocha";
 import { Card, cards, Deck, decks } from "../src/decks";
 
 describe("Create a new Card", () => {
-  it("should create a new Card object", () => {
+  it("should create a new default Card instance", () => {
     let card1: Card = new Card();
     expect(card1.id).to.not.equal("");
   });
 });
+
+let card1: Card;
+let card2: Card;
+let card3: Card;
+
+describe("Card Fields", () => {
+
+  beforeEach( () => {
+    card1 = new Card();
+    card2 = new Card();
+    card3 = new Card();
+  });
+
+  it("should update card state 'cards' array", () => {
+    //card1 = new Card();
+    card1.name = "Card 1"
+    expect( cards[card1.id] ).to.be.equal(card1);
+  });
+
+});
+
+/*
+describe("", () => {
+  beforeEach( () => {
+  });
+
+  it("", () => {
+  });
+});
+*/
