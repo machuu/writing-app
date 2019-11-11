59580938592c9423a4bd931d47a006a514f2477b
Matt Hoover <matt@hoover.ml>
2019-11-10 19:47:00 -0500

    lint: Fix indentation in `navigatorTest.ts`

--- test/navigationTest.ts
+++ test/navigationTest.ts
@@ -3,35 +3,33 @@ import "mocha";
 
 import NavMenu from "../src/navigation";
 
-
 // Initialize DOM
-this.jsdom = require('jsdom-global')();
+this.jsdom = require("jsdom-global")();
 
 describe("Creating a Navigator", () => {
 
+  // Initialize document elements
+  let navigatorContainer: HTMLElement    = document.createElement("div");
+  let navigatorCloseButton: HTMLElement  = document.createElement("div");
+  let navigatorOpenButton: HTMLElement   = document.createElement("div");
+  let navigatorToggleButton: HTMLElement = document.createElement("div");
 
-	// Initialize document elements
-	let navigatorContainer: HTMLElement    = document.createElement("div");
-	let navigatorCloseButton: HTMLElement  = document.createElement("div");
-	let navigatorOpenButton: HTMLElement   = document.createElement("div");
-	let navigatorToggleButton: HTMLElement = document.createElement("div");
+  beforeEach( () => {
+    // Add elements to body
+    document.body.appendChild(navigatorContainer);
+    document.body.appendChild(navigatorCloseButton);
+    document.body.appendChild(navigatorOpenButton);
+    document.body.appendChild(navigatorToggleButton);
 
-	beforeEach( () => {
-		// Add elements to body
-		document.body.appendChild(navigatorContainer);
-		document.body.appendChild(navigatorCloseButton);
-		document.body.appendChild(navigatorOpenButton);
-		document.body.appendChild(navigatorToggleButton);
-
-		// Assign element ids
-		navigatorContainer.id    = "navigator";
-		navigatorCloseButton.id  = `${navigatorContainer.id}-button-close`;
-		navigatorOpenButton.id   = `${navigatorContainer.id}-button-open`;
-		navigatorToggleButton.id = `${navigatorContainer.id}-button-toggle`;
-	});
+    // Assign element ids
+    navigatorContainer.id    = "navigator";
+    navigatorCloseButton.id  = `${navigatorContainer.id}-button-close`;
+    navigatorOpenButton.id   = `${navigatorContainer.id}-button-open`;
+    navigatorToggleButton.id = `${navigatorContainer.id}-button-toggle`;
+  });
 
   it("should create a new Navigator", () => {
-	  let navigator: NavMenu = new NavMenu(navigatorContainer.id);
+    let navigator: NavMenu = new NavMenu(navigatorContainer.id);
     expect(navigator.element).to.equal(navigatorContainer);
   });
 
@@ -41,8 +39,8 @@ describe("Creating a Navigator", () => {
   });
 
   it("should not auto assign an Close Button, if it the right element id doesn't exist", () => {
-		// Change id of button, so it doesn't match navigatorContainer.id
-		navigatorCloseButton.id = "foo";
+    // Change id of button, so it doesn't match navigatorContainer.id
+    navigatorCloseButton.id = "foo";
     let navigator: NavMenu = new NavMenu(navigatorContainer.id);
     expect(navigator.close_button).to.equal(undefined);
   });
@@ -53,8 +51,8 @@ describe("Creating a Navigator", () => {
   });
 
   it("should not auto assign an Open Button, if it the right element id doesn't exist", () => {
-		// Change id of button, so it doesn't match navigatorContainer.id
-		navigatorOpenButton.id = "foo";
+    // Change id of button, so it doesn't match navigatorContainer.id
+    navigatorOpenButton.id = "foo";
     let navigator: NavMenu = new NavMenu(navigatorContainer.id);
     expect(navigator.open_button).to.equal(undefined);
   });
@@ -65,8 +63,8 @@ describe("Creating a Navigator", () => {
   });
 
   it("should not auto assign an Toggle Button, if it the right element id doesn't exist", () => {
-		// Change id of button, so it doesn't match navigatorContainer.id
-		navigatorToggleButton.id = "foo";
+    // Change id of button, so it doesn't match navigatorContainer.id
+    navigatorToggleButton.id = "foo";
     let navigator: NavMenu = new NavMenu(navigatorContainer.id);
     expect(navigator.toggle_button).to.equal(undefined);
   });
