3ba624cbe68496e02e86aad5f447b8e2e31a441d
Matt Hoover <matt@hoover.ml>
2019-11-13 05:32:55 -0500

    feature: Add Accessors for `decks` and `deckIds`, Passing "Add Deck" test
    refs #24

--- src/project.ts
+++ src/project.ts
@@ -2,7 +2,21 @@ import { Card, cards, Deck, decks } from "./decks";
 
 export class Project extends Card {
   
+  private _deckIds: string[] = [];
+
   constructor() {
-    super()
+    super("Project")
+  }
+
+  public get deckIds(): string[] { return this._deckIds; }
+  public set deckIds(newDeckIds: string[]) { this._deckIds = newDeckIds; }
+
+  public get decks(): Deck[] {
+    return this.deckIds.map( (deckId: string): Deck => {
+      return decks[deckId];
+    });
+  }
+  public set decks(deckList: Deck[]) {
+    this.deckIds = deckList.map( (deck: Deck): string => { return deck.id; });
   }
 }
