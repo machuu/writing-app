a45f4b8333dac4d484f07fea5ab7cdfe87f3cdd9
Matt Hoover <matt@hoover.ml>
2019-11-14 05:14:00 -0500

    test: Migrate Project tests to Deck tests
    Copy tests from `test/projectTest.ts` -> `test/decksTest.ts`
    
    - `Deck` -> `Card`
    - `Project -> `Deck`
    
    refs #26

--- test/decksTest.ts
+++ test/decksTest.ts
@@ -42,6 +42,78 @@ describe("Card Fields", () => {
 
 });
 
+describe("Create a new Deck", () => {
+  it("should create a new Deck instance", () => {
+    let deck: Deck = new Deck("Scene");
+    expect(deck.id).to.not.equal("");
+  });
+});
+
+describe("A Deck has Cards", () => {
+  context("When some cards already exist", () => {
+    let deck1: Deck;
+    let card1: Card;
+    let card2: Card;
+
+    beforeEach( () => {
+      card1 = new Card("Scene");
+      card2 = new Card("Reference");
+    });
+
+    it("should be able to add one Card to a new Deck, using an array of cardIds", () => {
+      let deck1: Deck = new Deck();
+      let newCardIdList: string[] = [ card1.id ];
+      let newCardList: any = {};
+      newCardList[card1.id] = card1;
+
+      deck1.cardIds = newCardIdList;
+      expect(deck1.cardIds).to.be.deep.equal(newCardIdList);
+      expect(deck1.cards).to.be.deep.equal(newCardList);
+    });
+
+    it("should be able to add multiple Cards to a new Deck, using an array of cardIds", () => {
+      let deck1: Deck = new Deck();
+      let newCardIdList: string[] = [ card1.id, card2.id ];
+      let newCardList: any = {};
+      newCardList[card1.id] = card1;
+      newCardList[card2.id] = card2;
+
+      deck1.cardIds = newCardIdList;
+      expect(deck1.cardIds).to.be.deep.equal(newCardIdList);
+      expect(deck1.cards).to.include(newCardList);
+    });
+  });
+
+  context("When a Deck exists, with no Cards", () => {
+    // declare variables here, so deck1 and newCardId
+    // are shared between tests
+
+    let deck1: Deck;
+    let newCard: Card;
+    let newCardId: string;
+
+    before( () => {
+      deck1  = new Deck();
+      newCardId = deck1.newCard("Scene");
+      newCard   = deck1.cards[newCardId];
+    });
+
+    it("should be able to create a new Card, using Deck.newCard()", () => {
+      expect(deck1.cards).to.not.be.empty;
+      expect(deck1.cardIds).to.be.an('array').that.is.not.empty;
+    });
+
+    it("and new card is in Deck.cardIds", () => {
+      expect( deck1.cardIds ).to.be.an('array').that.includes( newCardId );
+    });
+
+    it("and new card is in global cards list", () => {
+      expect(cards).to.include(deck1.cards);
+    });
+  });
+});
+
+
 /*
 describe("", () => {
   beforeEach( () => {
