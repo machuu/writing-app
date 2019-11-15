786da3cfd8fa5f248be7bd5f922f54733e87d9e6
Matt Hoover <matt@hoover.ml>
2019-11-15 14:19:17 -0500

    feature: Refactor `class Project` to add getters for filtered deck lists
    refs #29

--- src/project.ts
+++ src/project.ts
@@ -16,30 +16,65 @@ export class Project extends BaseCard {
     Project._projects[this.id] = this;
   }
 
-  public get deckIds(): string[] { return this._deckIds; }
-  public set deckIds(newDeckIds: string[]) {
-    this._deckIds = newDeckIds;
-    this.updateGlobal();
-  }
-
-  public get decks(): any {
+  protected convertDeckIdsToDecks(deckIdList: string[]): any {
     let decksObject: any = {};
-    this.deckIds.forEach( (deckId: string) => {
+    deckIdList.forEach( (deckId: string) => {
       decksObject[deckId] = Deck.decks[deckId]
     });
     return decksObject;
   }
-
-  public set decks(decksObject: any) {
-    this.deckIds = decksObject.map( (deckId: string, deck: Deck): string => { return deckId; });
-    this.updateGlobal();
+  protected convertDecksToDeckIds(decksObject: any): string[] {
+    return decksObject.map( (deckId: string, deck: Deck): string => { return deckId; });
+  }
+  protected getFilteredDeckIds(filterString: string = ".*"): string[] {
+    let filterRegex = RegExp(filterString);
+    return this._deckIds.filter( (element) => {
+      return filterRegex.test(element);
+    });
   }
 
+  // Deck ID setter/getters
+  public set deckIds(newDeckIds: string[]) {
+    this._deckIds = newDeckIds;
+    this.updateGlobal();
+  }
+  public get deckIds(): string[] {
+    return this._deckIds;
+  }
+  public get sceneDeckIds(): string[] {
+    return this.getFilteredDeckIds("^Scene");
+  }
+  public get referenceDeckIds(): string[] {
+    return this.getFilteredDeckIds("^Reference");
+  }
+
+  // Deck setter/getters
+  public set decks(decksObject: any) {
+    this.deckIds = convertDecksToDeckIds(decksObject);
+    this.updateGlobal();
+  }
+  public get decks(): any {
+    return this.convertDeckIdsToDecks(this.deckIds);
+  }
+  public get sceneDecks(): any {
+    return this.convertDeckIdsToDecks(this.sceneDeckIds);
+  }
+  public get referenceDecks(): any {
+    return this.convertDeckIdsToDecks(this.referenceDeckIds);
+  }
+
+  // New Decks
   public newDeck(deckType: string): string {
     let newDeck: Deck = new Deck(deckType);
     this._deckIds.push(newDeck.id);
     this.updateGlobal();
     return newDeck.id;
+  }
+  public newReferenceDeck(): string[] {
+    return this.newDeck("Reference");
+  }
+  public newSceneDeck(): string[] {
+    return this.newDeck("Scene");
   }
 
   public addDeck(deck: Deck) {
@@ -50,6 +85,7 @@ export class Project extends BaseCard {
     this.updateGlobal();
   }
 
+  // Global Projects
   public static get projects(): any {
     return Project._projects;
   }
