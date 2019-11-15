3f99737f788d4b937f31ff201055ea0e26b74f62
Matt Hoover <matt@hoover.ml>
2019-11-15 06:50:34 -0500

    test: Add new failing tests for filtering type of returned decks from project
    refs #29

--- test/projectTest.ts
+++ test/projectTest.ts
@@ -86,6 +86,52 @@ describe("A Project has Decks", () => {
       expect(Deck.decks).to.include(project1.decks);
     });
   });
+
+  context("When a Project exists, with multiple decks", () => {
+    // declare variables here, so they are in scope during it()
+    // after being set in beforeEach()
+
+    let project1: Project;
+    let sceneDeck1: Deck;
+    let sceneDeck2: Deck;
+    let referenceDeck1: Deck;
+    let referenceDeck2: Deck;
+
+    beforeEach( () => {
+      project1  = new Project();
+      let sceneDeck1Id: string     = project1.newDeck("Scene");
+      let sceneDeck2Id: string     = project1.newDeck("Scene");
+      let referenceDeck1Id: string = project1.newDeck("Reference");
+      let referenceDeck2Id: string = project1.newDeck("Reference");
+      sceneDeck1       = project1.decks[sceneDeck1Id];
+      sceneDeck2       = project1.decks[sceneDeck2Id];
+      referenceDeck1   = project1.decks[referenceDeck1Id];
+      referenceDeck2   = project1.decks[referenceDeck2Id];
+    });
+
+    it("should only return Scene Decks", () => {
+      let sceneDeckIds: string[] = [];
+      sceneDeckIds.push(sceneDeck1.id);
+      sceneDeckIds.push(sceneDeck2.id);
+      let sceneDecksObject: any = {};
+      sceneDecksObject[sceneDeck1.id] = sceneDeck1;
+      sceneDecksObject[sceneDeck2.id] = sceneDeck2;
+      expect( project1.deckIds("Scene") ).to.be.deep.equal(sceneDeckIds);
+      expect( project1.decks("Scene")   ).to.be.deep.equal(sceneDecksObject);
+    });
+
+
+    it("should only return Reference Decks", () => {
+      let referenceDeckIds: string[] = [];
+      referenceDeckIds.push(referenceDeck1.id);
+      referenceDeckIds.push(referenceDeck2.id);
+      let referenceDecksObject: any = {};
+      referenceDecksObject[referenceDeck1.id] = referenceDeck1;
+      referenceDecksObject[referenceDeck2.id] = referenceDeck2;
+      expect( project1.deckIds("Reference") ).to.be.deep.equal(referenceDeckIds);
+      expect( project1.decks("Reference")   ).to.be.deep.equal(referenceDecksObject);
+    });
+  });
 });
 
 /*
